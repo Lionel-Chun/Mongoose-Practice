eb9483f0cb1995e19b02de76f7efccfac516e7a8
Lionel Chun <lionel.chun@outlook.com>
2024-11-12 03:31:13 +0800

    2024-11-12:03:31

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ mongoose.connect("mongodb://localhost:27017/exampleDB")
 .then(() => console.log('connected!'))
 .catch((e) => console.log(e));
 
+////// CRUD Begin
+
 // Step 1 Defining a Model with filter
 const studentSchema = new Schema({
     name: String,
@@ -21,27 +23,8 @@ const studentSchema = new Schema({
     }
 });
 
-// Step 2 Auto create a database named exampleDB Begin:
+// Step 2 Automatically create a database named exampleDB if it does not exist.
 const Student = mongoose.model("Student", studentSchema);
-
-
-app.get("/", async (req, res) => {
-    
-    res.setHeader('content-type', 'text/html;charset=utf-8');
-    res.write('<h1>使用Mongoose進行CRUD</h1>');
-    res.write('<p><a href="/create1">create1</a></p>');
-    res.write('<p><a href="/create2">create2</a></p>');
-    res.write('<p><a href="/create2?age=100">create2?age=100</a></p>');
-    res.write('<p><a href="/read1">read1</a></p>');
-    res.write('<p><a href="/read2">read2</a></p>');
-    res.write('<p><a href="/read3">read3</a></p>');
-    res.write('<p><a href="/read4">read4</a></p>');
-    res.write('<p><a href="/update1">update1</a></p>');
-    res.write('<p><a href="/update2">update2</a></p>');
-    res.write('<p><a href="/update3">update3</a> or <a href="/update4">update4</a></p>');
-    res.write('<p><a href="/delete1">delete1</a></p>');
-    res.end('再見');
-});
 
 app.get("/create1", async (req, res) => {
     // Step 3 Create a record 
@@ -203,8 +186,146 @@ app.get("/delete1", (req, res) => {
         res.send(e.message);
     });
 });
+////// CRUD End
 
 
+//// Schema Validators Begin
+
+// Define a schema using filters and create an object called studentSchema2.
+const studentSchema2 = new Schema({
+    name: {type: String, required: true},
+    age: { type: Number, min: [0, "年齡不能小於0歲"]},
+    major: {type: String, required: [true, "每個學生都需要選至少一個主修"]},
+    scholarship: {
+        merit: Number,
+        other: Number
+    }
+});
+
+
+// Automatically create a database named exampleDB if it does not exist.
+const Student2 = mongoose.model("Student2", studentSchema2);
+
+app.get("/create3", async (req, res) => {
+
+    const newObject2 = new Student2({
+        name: "Wilson",
+        age: 27,
+        // major: "Computer Sciense",
+        scholarship: {
+            merit: 3000,
+            other: 2000
+        }
+    });
+    newObject2.save().then(saveObject => {
+        if (saveObject === newObject2) {
+            console.log("資料已經儲存完畢，儲存的資料是：");
+            console.log(saveObject);
+            res.send(saveObject);
+        }
+    }).catch((e) => {
+        console.log(e.message);
+        res.send(e.message);
+    });
+});
+
+// Define a schema using filters and create an object called studentSchema2.
+const studentSchema3 = new Schema({
+    name: {type: String, required: true},
+    age: { type: Number, min: [0, "年齡不能小於0歲"]},
+    major: {type: String, required: [true, "每個學生都需要選至少一個主修"]},
+    scholarship: {
+        merit: {type: Number, max: [3000, "獎學金不得大於3000"]},
+        other: Number
+    }
+});
+
+// Automatically create a database named exampleDB if it does not exist.
+const Student3 = mongoose.model("Student3", studentSchema3);
+
+app.get("/create4", async (req, res) => {
+
+    const newObject3 = new Student3({
+        name: "Wilson",
+        age: 27,
+        major: "Computer Sciense",
+        scholarship: {
+            merit: 3000,
+            other: 2000
+        }
+    });
+    newObject3.save().then(saveObject => {
+        if (saveObject === newObject3) {
+            console.log("資料已經儲存完畢，儲存的資料是：");
+            console.log(saveObject);
+            res.send(saveObject);
+        }
+    }).catch((e) => {
+        console.log(e.message);
+        res.send(e.message);
+    });
+});
+
+const studentSchema4 = new Schema({
+    name: {type: String, required: true},
+    age: { type: Number, min: [0, "年齡不能小於0歲"]},
+    major: {type: String, required: [true, "每個學生都需要選至少一個主修"]},
+    scholarship: {
+        merit: {type: Number, default: 0},
+        other: {type: Number, default: 0}
+    }
+});
+
+// Automatically create a database named exampleDB if it does not exist.
+const Student4 = mongoose.model("Student4", studentSchema4);
+
+app.get("/create5", async (req, res) => {
+
+    const newObject4 = new Student4({
+        name: "Wilson",
+        age: 27,
+        major: "Computer Sciense",
+        // scholarship: {
+        //     merit: 3000,
+        //     other: 2000
+        // }
+    });
+    newObject4.save().then(saveObject => {
+        if (saveObject === newObject4) {
+            console.log("資料已經儲存完畢，儲存的資料是：");
+            console.log(saveObject);
+            res.send(saveObject);
+        }
+    }).catch((e) => {
+        console.log(e.message);
+        res.send(e.message);
+    });
+});
+//// Schema Validators End
+
+
+app.get("/", async (req, res) => {
+    
+    res.setHeader('content-type', 'text/html;charset=utf-8');
+    res.write('<h1>Mongoose</h1>');
+    res.write('<h2>CRUD</h2>')
+    res.write('<p><a href="/create1">create1</a></p>');
+    res.write('<p><a href="/create2">create2</a></p>');
+    res.write('<p><a href="/create2?age=100">create2?age=100</a></p>');
+    res.write('<p><a href="/read1">read1</a></p>');
+    res.write('<p><a href="/read2">read2</a></p>');
+    res.write('<p><a href="/read3">read3</a></p>');
+    res.write('<p><a href="/read4">read4</a></p>');
+    res.write('<p><a href="/update1">update1</a></p>');
+    res.write('<p><a href="/update2">update2</a></p>');
+    res.write('<p><a href="/update3">update3</a> or <a href="/update4">update4</a></p>');
+    res.write('<p><a href="/delete1">delete1</a></p>');
+    res.write('<h2>Schema Validators</h2>')
+    res.write('<p><a href="/create3">create3</a></p>');
+    res.write('<p><a href="/create4">create4</a></p>');
+    res.write('<p><a href="/create5">create5</a></p>');
+    res.end('再見');
+});
 
 app.listen(3000, () => {
     console.log("伺服器正在耹聽port 3000...")
